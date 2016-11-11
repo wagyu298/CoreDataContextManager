391858b96329deb9861c7661eef7405181599f58
wagyu298 <wagyu298@gmail.com>
2016-11-11 15:08:06 +0900

    Doxygen

--- CoreDataContextManager/Classes/CDMCoreDataContextManager.h
+++ CoreDataContextManager/Classes/CDMCoreDataContextManager.h
@@ -11,25 +11,82 @@ typedef NS_OPTIONS(NSUInteger, CDMCoreDataContextManagerOptions) {
     CDMCoreDataContextManagerOptionsDefault = 0,
 };
 
+/*!
+ @class CDMCoreDataContextManager
+ @brief NSManagedObjectContext wrapper with light weight migration and thread control
+ */
 @interface CDMCoreDataContextManager : NSObject
 
+/// @brief configuration passed from constructor */
 @property (nonnull, nonatomic, strong, readonly) CDMCoreDataContextManagerConfiguration*configuration;
+
+/// @brief A NSManagedObjectContext object that managed under CDMCoreDataContextManager
 @property (nonnull, nonatomic, strong, readonly) NSManagedObjectContext *managedObjectContext;
+
+/// @brief NSPersistentStoreCoordinator for creating managedObjectContext
 @property (nonnull, nonatomic, strong, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+/// @brief NSManagedObjectModel for creating managedObjectContext
 @property (nonnull, nonatomic, strong, readonly) NSManagedObjectModel *managedObjectModel;
 
+/*!
+ @brief Constructor with CDMCoreDataContextManagerConfiguration
+ @param configuration A configuration object
+ */
 - (instancetype _Nonnull)initWithConfiguration:(CDMCoreDataContextManagerConfiguration * _Nonnull)configuration;
 
+/*!
+ @brief Constructor with database name and auto save option
+ @param storeType Database store type string that provided by CoreData
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @param autoSave Execute @c[NSManagedObjectContext save:] when application will resign active
+ */
 - (instancetype _Nonnull)initWithStoreType:(NSString * _Nonnull)storeType databaseName:(NSString * _Nonnull)databaseName autoSave:(BOOL)autoSave;
+
+/*!
+ @brief Constructor with database name and enabled autoSave
+ @param storeType Database store type string that provided by CoreData
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithStoreType:(NSString * _Nonnull)storeType databaseName:(NSString * _Nonnull)databaseName;
 
+/*!
+ @brief Initialize SQLite database with database name and auto save option
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @param autoSave Execute @c[NSManagedObjectContext save:] when application will resign active
+ */
 - (instancetype _Nonnull)initWithSQLiteDatabaseName:(NSString * _Nonnull)databaseName autoSave:(BOOL)autoSave;
+
+/*!
+ @brief Initialize SQLite database with database name and enabled autoSave
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithSQLiteDatabaseName:(NSString * _Nonnull)databaseName;
 
+/*!
+ @brief Initialize Binary database with database name and auto save option
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @param autoSave Execute @c[NSManagedObjectContext save:] when application will resign active
+ */
 - (instancetype _Nonnull)initWithBinaryDatabaseName:(NSString * _Nonnull)databaseName autoSave:(BOOL)autoSave;
+
+/*!
+ @brief Initialize Binary database with database name and enabled autoSave
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithBinaryDatabaseName:(NSString * _Nonnull)databaseName;
 
+/*!
+ @brief Initialize InMemory database with database name and auto save option
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @param autoSave Execute @c[NSManagedObjectContext save:] when application will resign active
+ */
 - (instancetype _Nonnull)initWithInMemoryDatabaseName:(NSString * _Nonnull)databaseName autoSave:(BOOL)autoSave;
+
+/*!
+ @brief Initialize InMemory database with database name and enabled autoSave
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithInMemoryDatabaseName:(NSString * _Nonnull)databaseName;
 
 - (instancetype _Nonnull)initWithDatabaseName:(NSString * _Nonnull)databaseName directory:(NSURL * _Nullable)directory storeType:(NSString * _Nonnull)storeType options:(CDMCoreDataContextManagerOptions)options DEPRECATED_ATTRIBUTE;
@@ -37,9 +94,25 @@ typedef NS_OPTIONS(NSUInteger, CDMCoreDataContextManagerOptions) {
 - (instancetype _Nonnull)initWithDatabaseName:(NSString * _Nonnull)databaseName options:(CDMCoreDataContextManagerOptions)options DEPRECATED_ATTRIBUTE;
 - (instancetype _Nonnull)initWithDatabaseName:(NSString * _Nonnull)databaseName DEPRECATED_ATTRIBUTE;
 
+/*!
+ @brief Create NSManagedObjectContext to running background thread
+ @return NSManagedObjectContext object configured with background thread
+ @code
+ NSManagedObjectContext *ctx = [manager createBackgroundContext];
+ [ctx performBlock:^{
+     // This block run in background thread
+ }];
+ @endcode
+ */
 - (NSManagedObjectContext * _Nonnull)createBackgroundContext;
-- (NSManagedObjectContext * _Nonnull)currentManagedObjectContext;
 
+- (NSManagedObjectContext * _Nonnull)currentManagedObjectContext DEPRECATED_ATTRIBUTE;
+
+/*!
+ @brief Execute @c[managedObjectContext save:] if the managed object context has changes
+ @param error NSError instance if error occured
+ @return YES if success, otherwise NO
+ */
 - (BOOL)saveIfChanged:(NSError * _Nullable * _Nullable)error DEPRECATED_ATTRIBUTE;
 
 @end
--- CoreDataContextManager/Classes/CDMCoreDataContextManagerConfiguration.h
+++ CoreDataContextManager/Classes/CDMCoreDataContextManagerConfiguration.h
@@ -4,47 +4,165 @@
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+/*!
+ @class CDMCoreDataContextManagerConfiguration
+ @brief It provides CDMCoreDataContextManager configurations
+ */
 @interface CDMCoreDataContextManagerConfiguration : NSObject
 
+/// @brief storeType; one of the NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
 @property (nonatomic, strong, nonnull) NSString *storeType;
+
+/// @brief URL of a CoreData model file (.xcdatamodeld file)
 @property (nonatomic, strong, nonnull) NSURL *mappingModelURL;
+
+/// @brief URL for NSPersistentStoreCoordinator, it locates a real database file
 @property (nonatomic, strong, nonnull) NSURL *persistentStoreURL;
+
+/// @brief Enable auto save
 @property (nonatomic) BOOL autoSave;
+
+/// @brief Extra option for initialize NSPersistentStoreCoordinator
 @property (nonatomic, strong, nonnull) NSDictionary *storeOptions;
 
+/*!
+ @brief Initialize configuration with storeType
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ */
 - (instancetype _Nonnull)initWithStoreType:(NSString * _Nonnull)storeType;
+
+/*!
+ @brief Initialize configuration with storeType and database name
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithStoreType:(NSString * _Nonnull)storeType databaseName:(NSString * _Nonnull)databaseName;
 
+/*!
+ @brief Returns mappingModelURL with NSBundle and database name
+ @param bundle A bundle object
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @return URL for mappingModelURL
+ */
 - (NSURL * _Nonnull)mappingModelURLWithBundle:(NSBundle * _Nonnull)bundle databaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Returns mappingModelURL with mainBundle and database name
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @return URL for mappingModelURL
+ */
 - (NSURL * _Nonnull)mappingModelURLWithDatabaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Set mappingModelURL property with NSBundle and database name
+ @param bundle A bundle object
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (void)setMappingModelURLWithBundle:(NSBundle * _Nonnull)bundle databaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Set mappingModelURL property with mainBundle and database name
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (void)setMappingModelURLWithDatabaseName:(NSString * _Nonnull)databaseName;
 
+/*!
+ @brief Returns real database file name
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @return A database file name
+ */
 - (NSString * _Nullable)databaseFileNameWithStoreType:(NSString * _Nonnull)storeType databaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Returns persitent store URL
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param directoryURL A directory URL for persitent store
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @return URL for persistent store
+ */
 - (NSURL * _Nullable)persistentStoreURLWithStoreType:(NSString * _Nonnull)storeType directoryURL:(NSURL * _Nullable)directoryURL databaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Returns persitent store URL
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param URLsForDirectory NSSearchPathDirectory for persitent store
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @return URL for persistent store
+ */
 - (NSURL * _Nullable)persistentStoreURLWithStoreType:(NSString * _Nonnull)storeType URLsForDirectory:(NSSearchPathDirectory)URLsForDirectory databaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Returns persitent store URL with NSDocumentDirectory
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ @return URL for persistent store
+*/
 - (NSURL * _Nullable)persistentStoreURLWithStoreType:(NSString * _Nonnull)storeType databaseName:(NSString * _Nonnull)databaseName;
 
+/*!
+ @brief Set persitent store URL
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param directoryURL A directory URL for persitent store
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (void)setPersistentStoreURLWithStoreType:(NSString * _Nonnull)storeType directoryURL:(NSURL * _Nullable)directoryURL databaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Set persitent store URL
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param URLsForDirectory NSSearchPathDirectory for persitent store
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (void)setPersistentStoreURLWithStoreType:(NSString * _Nonnull)storeType URLsForDirectory:(NSSearchPathDirectory)URLsForDirectory databaseName:(NSString * _Nonnull)databaseName;
+
+/*!
+ @brief Set persitent store URL with NSDocumentDirectory
+ @param storeType NSSQLiteStoreType, NSBinaryStoreType or NSInMemoryStoreType
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (void)setPersistentStoreURLWithStoreType:(NSString * _Nonnull)storeType databaseName:(NSString * _Nonnull)databaseName;
 
 @end
 
+/*!
+ @class CDMCoreDataContextManagerSQLLiteConfiguration
+ @brief CDMCoreDataContextManager sub class for SQLite database
+ */
 @interface CDMCoreDataContextManagerSQLLiteConfiguration : CDMCoreDataContextManagerConfiguration
 
+/*!
+ @brief Initialize configuration with database name
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithDatabaseName:(NSString * _Nonnull)databaseName;
 
 @end
 
+/*!
+ @class CDMCoreDataContextManagerBinaryConfiguration
+ @brief CDMCoreDataContextManager sub class for Binary database
+ */
 @interface CDMCoreDataContextManagerBinaryConfiguration : CDMCoreDataContextManagerConfiguration
 
+/*!
+ @brief Initialize configuration with database name
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithDatabaseName:(NSString * _Nonnull)databaseName;
 
 @end
 
+/*!
+ @class CDMCoreDataContextManagerInMemoryConfiguration
+ @brief CDMCoreDataContextManager sub class for InMemory database
+ */
 @interface CDMCoreDataContextManagerInMemoryConfiguration : CDMCoreDataContextManagerConfiguration
 
+/*!
+ @brief Initialize configuration with database name
+ @param databaseName Database file name (prefix of .xcdatamodeld file)
+ */
 - (instancetype _Nonnull)initWithDatabaseName:(NSString * _Nonnull)databaseName;
 
 @end
