4d408c31505e1bf77914d63af7cb1da864cf59e9
wagyu298 <wagyu298@gmail.com>
2016-11-13 01:23:38 +0900

    Changed to migrate test with PRAGMA table_info and NSEntityDescription

--- Example/Tests/CDMCoreDataContextManagerTests.m
+++ Example/Tests/CDMCoreDataContextManagerTests.m
@@ -1,9 +1,48 @@
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 
+#import <string.h>
+#import <sqlite3.h>
 #import <CoreDataContextManager/CoreDataContextManager.h>
 
 #import "ExampleData+CoreDataClass.h"
+
+static NSMutableArray *tableInfoDest;
+
+static int
+eatTableInfo(void *data, int argc, char **argv, char **azColName)
+{
+    NSMutableDictionary *rowInfo = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < argc; i++) {
+        NSString *colName = [NSString stringWithCString:azColName[i] encoding:NSUTF8StringEncoding];
+        if (argv[i] != NULL) {
+            rowInfo[colName] = [NSString stringWithCString:argv[i] encoding:NSUTF8StringEncoding];
+        } else {
+            rowInfo[colName] = [NSNull null];
+        }
+    }
+    [tableInfoDest addObject:rowInfo];
+    return 0;
+}
+
+static NSArray *
+tableInfo(sqlite3 *db, NSString *tableName)
+{
+    NSString *sql = [NSString stringWithFormat:@"PRAGMA table_info(%@)", tableName];
+    tableInfoDest = [[NSMutableArray alloc] init];
+    
+    char *zErrMsg = 0;
+    int rc = sqlite3_exec(db, [sql cStringUsingEncoding:NSUTF8StringEncoding], eatTableInfo, NULL, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        NSLog(@"SQL error: %s", zErrMsg);
+        sqlite3_free(zErrMsg);
+        return nil;
+    }
+    
+    NSArray *rv = tableInfoDest;
+    tableInfoDest = nil;
+    return rv;
+}
 
 SpecBegin(CDMCoreDataContextManagerSpecs)
 
@@ -81,45 +120,70 @@ describe(@"CDMCoreDataContextManager", ^{
 });
 
 describe(@"Light weight migration", ^{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSString *sqlite3Path = [documentsPath stringByAppendingPathComponent:@"CoreDataContextManagerExample.sqlite"];
+    
     beforeAll(^{
-        NSFileManager *fileManager = [NSFileManager defaultManager];
-        NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+        NSError *error = nil;
+        BOOL rv = [fileManager removeItemAtPath:sqlite3Path error:&error];
+        expect(rv).to.beTruthy();
+        expect(error).to.beNil();
+        
         NSArray *files = @[
-                               @"CoreDataContextManagerExample.sqlite",
                                @"CoreDataContextManagerExample.sqlite-shm",
                                @"CoreDataContextManagerExample.sqlite-wal",
                                ];
         for (NSString *file in files) {
             NSString *filePath = [documentsPath stringByAppendingPathComponent:file];
-            NSError *error = nil;
             BOOL rv = [fileManager removeItemAtPath:filePath error:&error];
             expect(rv).to.beTruthy();
             expect(error).to.beNil();
         }
     });
+
+    it(@"before migrate", ^{
+        CDMCoreDataContextManagerConfiguration *config = [[CDMCoreDataContextManagerSQLLiteConfiguration alloc] initWithDatabaseName:@"CoreDataContextManagerExample"];
+        [config setMappingModelURLWithDatabaseName:@"CoreDataContextManagerExample_v1"];
+        
+        CDMCoreDataContextManager *manager = [[CDMCoreDataContextManager alloc] initWithConfiguration:config];
+        expect(manager).to.beTruthy();
+        
+        NSManagedObjectContext *context = manager.managedObjectContext;
+        NSManagedObject *data = [NSEntityDescription insertNewObjectForEntityForName:@"ExampleData" inManagedObjectContext:context];
+        [data setValue:@"title" forKey:@"title"];
+        [data setValue:@"section" forKey:@"section"];
+        [data setValue:@1 forKey:@"number"];
+        
+        NSEntityDescription *description = [data entity];
+        expect([description.properties count]).to.equal(3);
+        expect(description.properties[0].name).to.equal(@"number");
+        expect(description.properties[1].name).to.equal(@"section");
+        expect(description.properties[2].name).to.equal(@"title");
+        
+        NSError *error = nil;
+        BOOL rv = [context save:&error];
+        expect(rv).to.beTruthy();
+        expect(error).to.beNil();
+        
+        sqlite3 *db;
+        int rc = sqlite3_open([sqlite3Path fileSystemRepresentation], &db);
+        expect(rc == 0).to.beTruthy();
+        if (rc == 0) {
+            NSArray *info = tableInfo(db, @"ZEXAMPLEDATA");
+            expect(info).notTo.beNil();
+            expect([info count]).to.equal(6);
+            expect(info[0][@"name"]).to.equal(@"Z_PK");
+            expect(info[1][@"name"]).to.equal(@"Z_ENT");
+            expect(info[2][@"name"]).to.equal(@"Z_OPT");
+            expect(info[3][@"name"]).to.equal(@"ZNUMBER");
+            expect(info[4][@"name"]).to.equal(@"ZSECTION");
+            expect(info[5][@"name"]).to.equal(@"ZTITLE");
+            sqlite3_close(db);
+        }
+    });
     
     it(@"Migrate", ^{
-        waitUntil(^(DoneCallback done) {
-            CDMCoreDataContextManagerConfiguration *config = [[CDMCoreDataContextManagerSQLLiteConfiguration alloc] initWithDatabaseName:@"CoreDataContextManagerExample"];
-            [config setMappingModelURLWithDatabaseName:@"CoreDataContextManagerExample_v1"];
-            
-            CDMCoreDataContextManager *manager = [[CDMCoreDataContextManager alloc] initWithConfiguration:config];
-            expect(manager).to.beTruthy();
-            
-            NSManagedObjectContext *context = manager.managedObjectContext;
-            NSManagedObject *data = [NSEntityDescription insertNewObjectForEntityForName:@"ExampleData" inManagedObjectContext:context];
-            [data setValue:@"title" forKey:@"title"];
-            [data setValue:@"section" forKey:@"section"];
-            [data setValue:@1 forKey:@"number"];
-            
-            NSError *error = nil;
-            BOOL rv = [context save:&error];
-            expect(rv).to.beTruthy();
-            expect(error).to.beNil();
-            
-            done();
-        });
-        
         CDMCoreDataContextManagerConfiguration *config = [[CDMCoreDataContextManagerSQLLiteConfiguration alloc] initWithDatabaseName:@"CoreDataContextManagerExample"];
         
         CDMCoreDataContextManager *manager = [[CDMCoreDataContextManager alloc] initWithConfiguration:config];
@@ -134,9 +198,35 @@ describe(@"Light weight migration", ^{
         expect(error).to.beNil();
         expect([results count]).to.equal(1);
         
-        ExampleData *data = results[0];
-        expect(data.title).to.equal(@"title");
-        expect(data.updatedAt).to.beNil();
+        NSManagedObject *data = results[0];
+        expect([data valueForKey:@"title"]).to.equal(@"title");
+        expect([data valueForKey:@"section"]).to.equal(@"section");
+        expect([data valueForKey:@"number"]).to.equal(@1);
+        expect([data valueForKey:@"updatedAt"]).to.beNil();
+        
+        NSEntityDescription *description = [data entity];
+        expect([description.properties count]).to.equal(4);
+        expect(description.properties[0].name).to.equal(@"number");
+        expect(description.properties[1].name).to.equal(@"section");
+        expect(description.properties[2].name).to.equal(@"title");
+        expect(description.properties[3].name).to.equal(@"updatedAt");
+        
+        sqlite3 *db;
+        int rc = sqlite3_open([sqlite3Path fileSystemRepresentation], &db);
+        expect(rc == 0).to.beTruthy();
+        if (rc == 0) {
+            NSArray *info = tableInfo(db, @"ZEXAMPLEDATA");
+            expect(info).notTo.beNil();
+            expect([info count]).to.equal(7);
+            expect(info[0][@"name"]).to.equal(@"Z_PK");
+            expect(info[1][@"name"]).to.equal(@"Z_ENT");
+            expect(info[2][@"name"]).to.equal(@"Z_OPT");
+            expect(info[3][@"name"]).to.equal(@"ZNUMBER");
+            expect(info[4][@"name"]).to.equal(@"ZUPDATEDAT");
+            expect(info[5][@"name"]).to.equal(@"ZSECTION");
+            expect(info[6][@"name"]).to.equal(@"ZTITLE");
+            sqlite3_close(db);
+        }
     });
 
 });
