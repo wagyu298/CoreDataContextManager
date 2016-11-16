0966fe09af859359d377eec2dd6b7b7dac0f083a
wagyu298 <wagyu298@gmail.com>
2016-11-16 18:58:28 +0900

    Doxygen

--- CoreDataContextManager/Classes/CDMFetchedResultsControllerDelegate.h
+++ CoreDataContextManager/Classes/CDMFetchedResultsControllerDelegate.h
@@ -4,21 +4,53 @@
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
+/*!
+ @protocol CDMFetchedResultsControllerDelegateDataSource
+ @discussion Delegate for CDMFetchedResultsControllerDelegate.
+ tableView.dataSource should implement this protocol.
+ */
 @protocol CDMFetchedResultsControllerDelegateDataSource <UITableViewDataSource>
 
+
+/*!
+ @brief configure UITableViewCell
+ @param cell A cell for configuring
+ @param indexPath indexPath of cell
+ */
 @required
 - (void)configureCell:(UITableViewCell * _Nonnull)cell atIndexPath:(NSIndexPath * _Nullable)indexPath;
 
 @end
 
+/*!
+ @class CDMFetchedResultsControllerDelegate
+ @brief Example implementation of NSFetchedResultsControllerDelegate
+ */
 @interface CDMFetchedResultsControllerDelegate : NSObject <NSFetchedResultsControllerDelegate>
 
+/*!
+ @brief Change default animation for row update
+ @param rowAnimation row update animation
+ */
 + (void)setDefaultRowAnimation:(UITableViewRowAnimation)rowAnimation;
 
+/// @brief UITableView for managing by NSFetchedResultsController
 @property (nullable, nonatomic, weak) IBOutlet UITableView *tableView;
+
+/// @brief row update animation
 @property (nonatomic) UITableViewRowAnimation rowAnimation;
 
+/*!
+ @brief Initialize CDMFetchedResultsControllerDelegate with table view and row animation
+ @param tableView table view for managing by NSFetchedResultsController
+ @param rowAnimation row update animation
+ */
 - (instancetype _Nonnull)initWithTableView:(UITableView * _Nonnull)tableView rowAnimation:(UITableViewRowAnimation)rowAnimation;
+
+/*!
+ @brief Initialize CDMFetchedResultsControllerDelegate with table view and row animation
+ @param tableView table view for managing by NSFetchedResultsController
+ */
 - (instancetype _Nonnull)initWithTableView:(UITableView * _Nonnull)tableView;
 
 @end
--- CoreDataContextManager/Classes/NSManagedObjectContext+CoreDataContextManager.h
+++ CoreDataContextManager/Classes/NSManagedObjectContext+CoreDataContextManager.h
@@ -3,10 +3,30 @@
 
 #import <CoreData/CoreData.h>
 
+/**
+ CoreData extensions
+ */
 @interface NSManagedObjectContext (CoreDataContextManager)
 
+/*!
+ @brief Create child managed object context for multithreading
+ @return A NSManagedObjectContext object
+ */
 - (NSManagedObjectContext * _Nonnull)cdm_createChildManagedObjectContext;
+
+/*!
+ @brief save if NSManagedObjectContext has changed
+ @param error A error pointer
+ @return YES if success, otherwise NO
+ */
 - (BOOL)cdm_saveChanges:(NSError * _Nullable * _Nullable)error;
+
+/*!
+ @brief Delete all entity objects
+ @param entityName Entity name to delete all objects
+ @param error A error pointer
+ @return YES if success, otherwise NO
+ */
 - (BOOL)cdm_deleteWithEntityName:(NSString * _Nonnull)entityName error:(NSError * _Nullable * _Nullable)error;
 
 @end
