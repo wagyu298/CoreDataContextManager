9ccf284f3a6af7b0c4809eafab96fa138355e7b0
wagyu298 <wagyu298@gmail.com>
2016-11-16 19:21:29 +0900

    Header file import path changed

--- CoreDataContextManager/Classes/CoreDataContextManager.h
+++ CoreDataContextManager/Classes/CoreDataContextManager.h
@@ -1,7 +1,7 @@
 // This is free and unencumbered software released into the public domain.
 // For more information, please refer to <http://unlicense.org/>
 
-#import "CDMCoreDataContextManager.h"
-#import "CDMCoreDataContextManagerConfiguration.h"
-#import "CDMFetchedResultsControllerDelegate.h"
-#import "NSManagedObjectContext+CoreDataContextManager.h"
+#import <CoreDataContextManager/CDMCoreDataContextManager.h>
+#import <CoreDataContextManager/CDMCoreDataContextManagerConfiguration.h>
+#import <CoreDataContextManager/CDMFetchedResultsControllerDelegate.h>
+#import <CoreDataContextManager/NSManagedObjectContext+CoreDataContextManager.h>
